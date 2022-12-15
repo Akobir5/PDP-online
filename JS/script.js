8b1f0d73739105a106573a85d5dd7dd5f2364dba
Akobir5 <toshtemirovakobir8@gmail.com>
2022-12-15 11:22:22 +0500

    add popup

--- JS/script.js
+++ JS/script.js
@@ -3,29 +3,28 @@ let btnVideo = document.querySelector('.btnVideo');
 let closeBtn = document.querySelector('.closeBtn');
 let header = document.querySelector('.header');
 let iframeVideo = document.querySelector('.iframeVideo');
+let closeBox = document.querySelector('.closeBox');
 
 btnVideo.addEventListener('click', () => {
   videoBox.classList.remove('hideBox');
-  // videoBox.classList.add('show');
   if(iframeVideo.classList.contains('addVideo')) {
     iframeVideo.setAttribute('src', 'https://www.youtube.com/embed/IlVpkBf3McU');
   }
 })
 
 closeBtn.addEventListener('click', () => {
+  closeVideo()
+})
+
+closeBox.addEventListener('click', () => {
+  closeVideo()
+})
+
+function closeVideo() {
   videoBox.classList.add('hideBox');
   iframeVideo.removeAttribute('src');
   iframeVideo.classList.add('addVideo');
-})
-
-// window.addEventListener('click', ({ target }) => {
-//   const popup = target.closest('.videoBox');
-//   const clickedOnClosedPopup = popup && !popup.classList.contains('show');
-  
-//   popup.classList.remove('show');
-  
-//   if (clickedOnClosedPopup) popup.classList.add('show');  
-// });
+}
 
 window.addEventListener('scroll', () => {
   if(scrollY == "20") {
